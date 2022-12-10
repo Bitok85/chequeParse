333329087a1f1d8af762a70757993632d1536216
Bitok85 <kan041185@gmail.com>
2022-12-10 11:59:35 +0300

    initial commit

---
+++ src/main/java/ru/chequeparse/ChequeParseApplication.java
@@ -0,0 +1,13 @@
+package ru.chequeparse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChequeParseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChequeParseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/chequeparse/ChequeParseApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.chequeparse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChequeParseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
